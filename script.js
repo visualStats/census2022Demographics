bb434867d2b68aab420da1844c4535658cd9b7b1
visualStats <48520438+visualStats@users.noreply.github.com>
2025-02-12 15:24:13 +0000

    Updates

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
     // Your code here
     renderCountySelect();
     $('#select-county,#select-sex,#select-age ').change(renderCountyMap);
+    $("#download-ed-data").on("click", downloadCountyCsv);
+    $("#download-sa-data").on("click", downloadEdCsv);
 
-    var myModalEl = document.getElementById('ed-modal')
-
-    myModalEl.addEventListener('hide.bs.modal', function () {
-        // $('#ed-map-wrapper').empty();
-    });
 });
+
+var countyMapConfig = {};
+var edMapConfig = {};
+var edSelectedName = "";
 
 renderCountySelect = function () {
     config.counties.sort(function (a, b) {
@@ -20,7 +21,7 @@ renderCountySelect = function () {
             text: county.name
         }));
     });
-}
+};
 
 renderCountyMap = function () {
     $("#spinner-overlay").fadeIn(); // Show the spinner overlay
@@ -30,72 +31,68 @@ renderCountyMap = function () {
     //get a list of ed'd for more efficient query
 
     $.getJSON("maps/counties/" + $("#select-county").val() + ".geojson", function (data) {
-        var mapConfig = $.extend(true, {}, config.countyMap);
+        countyMapConfig = $.extend(true, {}, config.countyMap);
         $.each(data.features, function (index, feature) {
-            mapConfig.data.datasets[0].api.query.data.params.dimension["C04167V04938"].category.index.push(feature.properties.ED_GUID);
+            countyMapConfig.data.datasets[0].api.query.data.params.dimension["C04167V04938"].category.index.push(feature.properties.ED_GUID);
         });
 
-        mapConfig.options.geojson = "maps/counties/" + $("#select-county").val() + ".geojson";
-        mapConfig.data.datasets[0].api.query.data.params.dimension["C03738V04487"].category.index = [$("#select-sex").val()];
-        mapConfig.data.datasets[0].api.query.data.params.dimension["C03737V04485"].category.index = [$("#select-age").val()];
-        mapConfig.tooltipTitle = $("#select-sex option:selected").text() + " : " + $("#select-age option:selected").text();
-        pxWidget.draw.init("map", "county-map", mapConfig, function () {
+        countyMapConfig.options.geojson = "maps/counties/" + $("#select-county").val() + ".geojson";
+        countyMapConfig.data.datasets[0].api.query.data.params.dimension["C03738V04487"].category.index = [$("#select-sex").val()];
+        countyMapConfig.data.datasets[0].api.query.data.params.dimension["C03737V04485"].category.index = [$("#select-age").val()];
+        countyMapConfig.tooltipTitle = $("#select-sex option:selected").text() + " : " + $("#select-age option:selected").text();
+        pxWidget.draw.init("map", "county-map", countyMapConfig, function () {
             $("#spinner-overlay").fadeOut(); // Hide spinner after AJAX completes
         });
-
-        $("#download-ed-data").one("click", function () {
-            $("#spinner-overlay").fadeIn(); // Show the spinner overlay
-            var csvConfig = $.extend(true, {}, mapConfig.data.datasets[0].api.query.data);
-            csvConfig.params.extension.format = {
-                "type": "CSV",
-                "version": "1.0"
-            };
-
-
-            $.ajax({
-                url: "https://ws.cso.ie/public/api.jsonrpc",
-                type: "POST",
-                dataType: "json",
-                data: JSON.stringify(csvConfig),
-                success: function (response) {
-                    const csvData = response.result; // Assuming response.result contains CSV text
-                    const blob = new Blob([csvData], { type: "text/csv" });
-                    const url = URL.createObjectURL(blob);
-                    const a = document.createElement("a");
-                    a.href = url;
-                    a.download = $("#select-county option:selected").text() + ".csv"; // File name
-                    document.body.appendChild(a);
-                    a.click();
-                    document.body.removeChild(a);
-                    URL.revokeObjectURL(url);
-                },
-                complete: function () {
-                    $("#spinner-overlay").fadeOut(); // Hide spinner after AJAX completes 
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-
-                }
-            });
-
-
-
-        })
     });
 
 
+};
+
+downloadCountyCsv = function () {
+    $("#spinner-overlay").fadeIn(); // Show the spinner overlay
+    var csvConfig = $.extend(true, {}, countyMapConfig.data.datasets[0].api.query.data);
+    csvConfig.params.extension.format = {
+        "type": "CSV",
+        "version": "1.0"
+    };
+    $.ajax({
+        url: "https://ws.cso.ie/public/api.jsonrpc",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify(csvConfig),
+        success: function (response) {
+            const csvData = response.result; // Assuming response.result contains CSV text
+            const blob = new Blob([csvData], { type: "text/csv" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = $("#select-county option:selected").text() + ".csv"; // File name
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        },
+        complete: function () {
+            $("#spinner-overlay").fadeOut(); // Hide spinner after AJAX completes 
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+        }
+    });
 }
+
 renderEdMap = function (guid, areaName, value) {
+    edSelectedName = areaName;
     $("#spinner-overlay").fadeIn(); // Show the spinner overlay
     $('#ed-map').empty();
 
     //get a list of ed'd for more efficient query
 
     $.getJSON("maps/eds/" + guid + ".geojson", function (data) {
-        var edMapConfig = $.extend(true, {}, config.edMap);
+        edMapConfig = $.extend(true, {}, config.edMap);
         var myModal = new bootstrap.Modal(document.getElementById('ed-modal'));
-        $("#ed-modal .modal-title").text(areaName + " - " + $("#select-sex option:selected").text() + " : " + $("#select-age option:selected").text() + " (Total : "+value+")");
+        $("#ed-modal .modal-title").text(areaName + " - " + $("#select-sex option:selected").text() + " : " + $("#select-age option:selected").text() + " (Total : " + value + ")");
         myModal.show();
-        var edMapConfig = $.extend(true, {}, config.edMap);
         $.each(data.features, function (index, feature) {
             edMapConfig.data.datasets[0].api.query.data.params.dimension["C04172V04943"].category.index.push(feature.properties.SA_GUID_2022);
         });
@@ -106,43 +103,40 @@ renderEdMap = function (guid, areaName, value) {
         pxWidget.draw.init("map", "ed-map", edMapConfig, function () {
             $("#spinner-overlay").fadeOut(); // Hide spinner after AJAX completes
         });
-
-        $("#download-sa-data").one("click", function () {
-            $("#spinner-overlay").fadeIn(); // Show the spinner overlay
-            var csvConfig = $.extend(true, {}, edMapConfig.data.datasets[0].api.query.data);
-            csvConfig.params.extension.format = {
-                "type": "CSV",
-                "version": "1.0"
-            };
-
-
-            $.ajax({
-                url: "https://ws.cso.ie/public/api.jsonrpc",
-                type: "POST",
-                dataType: "json",
-                data: JSON.stringify(csvConfig),
-                success: function (response) {
-                    const csvData = response.result; // Assuming response.result contains CSV text
-                    const blob = new Blob([csvData], { type: "text/csv" });
-                    const url = URL.createObjectURL(blob);
-                    const a = document.createElement("a");
-                    a.href = url;
-                    a.download = areaName.replace(/[ ,]/g, '_').toLowerCase() + "_" + $("#select-sex option:selected").text().toLowerCase() + "_" + $("#select-age option:selected").text().toLowerCase() + ".csv"; // File name
-                    document.body.appendChild(a);
-                    a.click();
-                    document.body.removeChild(a);
-                    URL.revokeObjectURL(url);
-                },
-                complete: function () {
-                    $("#spinner-overlay").fadeOut(); // Hide spinner after AJAX completes 
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-
-                }
-            });
-
-
-
-        })
     });
-}
+};
+
+downloadEdCsv = function () {
+    $("#spinner-overlay").fadeIn(); // Show the spinner overlay
+    var csvConfig = $.extend(true, {}, edMapConfig.data.datasets[0].api.query.data);
+    csvConfig.params.extension.format = {
+        "type": "CSV",
+        "version": "1.0"
+    };
+
+
+    $.ajax({
+        url: "https://ws.cso.ie/public/api.jsonrpc",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify(csvConfig),
+        success: function (response) {
+            const csvData = response.result; // Assuming response.result contains CSV text
+            const blob = new Blob([csvData], { type: "text/csv" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = edSelectedName.replace(/[ ,]/g, '_').toLowerCase() + "_" + $("#select-sex option:selected").text().toLowerCase() + "_" + $("#select-age option:selected").text().toLowerCase() + ".csv"; // File name
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        },
+        complete: function () {
+            $("#spinner-overlay").fadeOut(); // Hide spinner after AJAX completes 
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+        }
+    });
+};
